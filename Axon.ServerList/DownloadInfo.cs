25eb525c89fad3134572900baba6389f6bb141ee
GrafDimenzio <66207264+GrafDimenzio@users.noreply.github.com>
2024-07-19 17:49:10 +0200

    Projektdateien hinzufügen.

---
+++ Axon.ServerList/DownloadInfo.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Axon.ServerList;
+
+[Serializable]
+public class ModInfo
+{
+    [JsonProperty("name")] public string Name = "";
+    [JsonProperty("version")] public string Version = "";
+}
